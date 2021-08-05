1c64252a7a53f50b50c8dd69cd4ce0a8710f38da
Will <28814921+wkey1980@users.noreply.github.com>
2021-08-05 13:28:12 +0100

    Started to build App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../Navbar/index';
+import './styles/App.Styles.css';
+
+function App() {
+	return (
+		<div className="App">
+			<Navbar />
+		</div>
+	);
+}
+
+export default App;
