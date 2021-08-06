9d3dd118aa28b7791cf7f4c8c97a73fbd02c8d13
Will <28814921+wkey1980@users.noreply.github.com>
2021-08-06 20:08:31 +0100

    Start to build the navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import './styles/Navbar.Styles.css'
 
 const Navbar = () => {
 	return (
 		<div>
 			<h3>Navbar Component</h3>
+			<nav className="navbar">
+				<ul className="navbar__left">
+					<li><a href="#">About</a></li>
+					<li><a href="#">Store</a></li>
+				</ul>
+
+				<ul className="navbar__right">
+					<li><a href="#">Gmail</a></li>
+					<li><a href="#">Images</a></li>
+					<li><a href="#">9dots</a></li>
+					<li><a href="#">Sign in</a></li>
+				</ul>
+
+			</nav>
 		</div>
 	)
 }
